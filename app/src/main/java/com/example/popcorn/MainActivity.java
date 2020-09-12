04be99997b3fe75ddf3674c598ec03eebdbedec1
Cameron <cameron@aheadofthecurve.co.za>
2020-09-12 10:13:09 +0200

    Updated the UI.
    - ComingSoonFrag: Added this fragment to display all of the coming soon
    torrents.
    - MainActivity: Updated the UI.
    - drawable: Added blank icons for nav bar.

--- app/src/main/java/com/example/popcorn/MainActivity.java
+++ app/src/main/java/com/example/popcorn/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.popcorn;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.popcorn.menu.ComingSoonFrag;
 import com.example.popcorn.menu.HomeFrag;
 import com.example.popcorn.menu.MenuFrag;
 import com.example.popcorn.menu.RefreshFrag;
@@ -14,6 +16,12 @@ import com.example.popcorn.utils.FragmentUtils;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private ImageButton btnSearch;
+    private ImageButton btnRefresh;
+    private ImageButton btnHome;
+    private ImageButton btnMenu;
+    private ImageButton btnComingSoon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +32,40 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        HomeFrag homeFrag = new HomeFrag();
-        FragmentUtils.startFragment(getSupportFragmentManager(), homeFrag, R.id.fragContainer, getSupportActionBar(), "Home", true, false, true, null);
-
         addBtnRefreshListener();
         addBtnSearchListener();
         addBtnHomeListener();
         addBtnMenuListner();
+        addBtnComingSoon();
+
+        HomeFrag homeFrag = new HomeFrag();
+        FragmentUtils.startFragment(getSupportFragmentManager(), homeFrag, R.id.fragContainer, getSupportActionBar(), "Home", true, false, true, null);
+
+        setNavIcons(true, false, false, false);
+    }
+
+    private void addBtnComingSoon()
+    {
+        this.btnComingSoon = (ImageButton) findViewById(R.id.btnComingSoon);
+        this.btnComingSoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNavIcons(false, false, false, false);
+
+                ComingSoonFrag comingSoonFrag = new ComingSoonFrag();
+                FragmentUtils.startFragment(getSupportFragmentManager(), comingSoonFrag, R.id.fragContainer, getSupportActionBar(), "Coming Soon", true, false, true, null);
+            }
+        });
     }
 
     private void addBtnRefreshListener()
     {
-        ImageButton btnRefresh = findViewById(R.id.btnRefresh);
+        this.btnRefresh = (ImageButton) findViewById(R.id.btnRefresh);
         btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
+                setNavIcons(false, false, true, false);
                 RefreshFrag refreshFrag = new RefreshFrag();
                 FragmentUtils.startFragment(getSupportFragmentManager(), refreshFrag, R.id.fragContainer, getSupportActionBar(), "Refresh Movies", true, false, true, null);
             }
@@ -48,10 +74,11 @@ public class MainActivity extends AppCompatActivity
 
     private void addBtnSearchListener()
     {
-        ImageButton btnSearch = findViewById(R.id.btnSearch);
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        this.btnSearch = findViewById(R.id.btnSearch);
+        this.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setNavIcons(false, true, false, false);
                 SearchFrag searchFrag = new SearchFrag();
                 FragmentUtils.startFragment(getSupportFragmentManager(), searchFrag,R.id.fragContainer, getSupportActionBar(), "Search", true, false, true, null);
             }
@@ -60,10 +87,11 @@ public class MainActivity extends AppCompatActivity
 
     private void addBtnHomeListener()
     {
-        ImageButton btnHome = findViewById(R.id.btnHome);
-        btnHome.setOnClickListener(new View.OnClickListener() {
+        this.btnHome = findViewById(R.id.btnHome);
+        this.btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setNavIcons(true, false, false, false);
                 HomeFrag homeFrag = new HomeFrag();
                 FragmentUtils.startFragment(getSupportFragmentManager(), homeFrag, R.id.fragContainer, getSupportActionBar(), "Home", true, false, true, null);
             }
@@ -72,13 +100,50 @@ public class MainActivity extends AppCompatActivity
 
     private void addBtnMenuListner()
     {
-        ImageButton btnMenu = findViewById(R.id.btnMenu);
-        btnMenu.setOnClickListener(new View.OnClickListener() {
+        this.btnMenu = findViewById(R.id.btnMenu);
+        this.btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setNavIcons(false, false, false, true);
                 MenuFrag menuFrag = new MenuFrag();
                 FragmentUtils.startFragment(getSupportFragmentManager(), menuFrag, R.id.fragContainer, getSupportActionBar(), "Menu", true, false, true, null);
             }
         });
+    }
+
+    public void setNavIcons(boolean isHome, boolean isSearch, boolean isRefresh, boolean isMenu)
+    {
+        if(isHome)
+        {
+            this.btnHome.setBackgroundResource(R.drawable.home);
+        }else
+        {
+            this.btnHome.setBackgroundResource(R.drawable.home_blank);
+        }
+
+        if(isSearch)
+        {
+            this.btnSearch.setBackgroundResource(R.drawable.search);
+        }else
+        {
+            this.btnSearch.setBackgroundResource(R.drawable.search_blank);
+        }
+
+        if(isRefresh)
+        {
+            this.btnRefresh.setBackgroundResource(R.drawable.refresh);
+        }else
+        {
+            this.btnRefresh.setBackgroundResource(R.drawable.refresh_blank);
+        }
+
+        if(isMenu)
+        {
+            this.btnMenu.setBackgroundResource(R.drawable.menu);
+        }else
+        {
+            this.btnMenu.setBackgroundResource(R.drawable.menu_blank);
+        }
+
     }
 }
